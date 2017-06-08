e9a8368724af0ac320a8eb66bfe8efbc625ed184
cuong.bui <cuongbm0211@gmail.com>
2017-06-08 15:08:36 +0700

    Init project

--- src/main/scala/com/study/scala/http/server/WebServer.scala
+++ src/main/scala/com/study/scala/http/server/WebServer.scala
@@ -3,7 +3,7 @@ package com.study.scala.http.server
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives
+import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 import scala.io.StdIn
 
